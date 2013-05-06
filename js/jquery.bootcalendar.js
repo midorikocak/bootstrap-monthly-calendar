fb07f124c324220bdb0d639f447f455e59bf30e8
Mutlu Tevfik Kocak <mtkocak@gmail.com>
2013-05-06 11:38:15 +0200

    Refactored functions

--- js/jquery.bootcalendar.js
+++ js/jquery.bootcalendar.js
@@ -1,49 +1,93 @@
 (function( $ ){
 
-    function getFirstDay (date,month,year) {
-
-        var dateObj =  new Date();
-        dateObj.setFullYear(year);
-        //alert(dateObj.getFullYear());
-
-        month_names = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
-        dateObj.setMonth(month);
-        var m_name = dateObj.getMonth();
-        //alert(month_names[m_name]);
-
-        day_names = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-        dateObj.setDate(1);
-        var first_day = dateObj.getDay();
-
-        return first_day;
-    }
-
-    function monthTable(firstWeekDay,previousMonthLastDay,lastDay,today,month,year)
+    function monthTable(day, month, year)
     {
-        var startDay = previousMonthLastDay - (firstWeekDay-2);
-        var counter = 1;
+        var lastDay = new Date(year, month + 1, 0).getDate();
+        
+        var firstWeekDay = new Date(year, month, 0).getDay();
+
+        var previousMonthLastDay = new Date(year, month, 0).getDate();
+
+        if(month!=new Date().getMonth())
+        {
+            var today = 0;
+        }
+        else
+        {
+            var today = day;
+        }
+        
+        var startDay = previousMonthLastDay - (firstWeekDay);
+
+        var counter = 0;
 
         var month_names = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+        
         var currentMonth = month_names[month];
 
-        var output ='<table class="table-condensed table-bordered table-striped">\n\t<thead>\n\t\t<tr>\n\t\t\t<th colspan="7">\n\t\t\t\t<span class="btn-group">\n\t\t\t\t\t<a id="left" class="btn"><i class="icon-chevron-left"></i></a>\n\t\t\t\t\t<a id="rigth" class="btn active">';
-        output += currentMonth + " "+year+'</a>\n\t\t\t\t\t<a class="btn"><i class="icon-chevron-right"></i></a>\n\t\t\t\t</span>\n\t\t\t</th>\n\t\t</tr>\n\t\t<tr>\n\t\t\t<th>Su</th>\n\t\t\t<th>Mo</th>\n\t\t\t<th>Tu</th>\n\t\t\t<th>We</th>\n\t\t\t<th>Th</th>\n\t\t\t<th>Fr</th>\n\t\t\t<th>Sa</th>\n\t\t</tr>\n\t</thead>\n\t<tbody>\n';
+        var output ='<table class="table-condensed table-bordered table-striped">\n\t<thead>\n\t\t<tr>\n\t\t\t<th colspan="7">\n\t\t\t\t<span class="btn-group">\n\t\t\t\t\t<a id="left" class="btn"><i class="icon-chevron-left"></i></a>\n\t\t\t\t\t<a class="btn active">';
+        output += currentMonth + " "+year+'</a>\n\t\t\t\t\t<a id="right"  class="btn"><i class="icon-chevron-right"></i></a>\n\t\t\t\t</span>\n\t\t\t</th>\n\t\t</tr>\n\t\t<tr>\n\t\t\t<th>Mo</th>\n\t\t\t<th>Tu</th>\n\t\t\t<th>We</th>\n\t\t\t<th>Th</th>\n\t\t\t<th>Fr</th>\n\t\t\t<th>Sa</th>\n\t\t\t<th>Su</th>\n\t\t</tr>\n\t</thead>\n\t<tbody>\n';
+        
         var monthNumbers = startDay;
+        
         output += '\t\t<tr>\n';
+        
         var tdOpener = "<td>"
+        
         var resetCounter = 0;
-        while(counter<36)
+        
+        //console.debug(startDay,previousMonthLastDay,firstWeekDay,lastDay)
+        
+        if(firstWeekDay == 6)
         {
-            if(monthNumbers==lastDay+1)
+            var limit = 42;
+        }
+        else
+        {
+            var limit = 35;
+        }
+        
+        while(counter<limit)
+        {
+            if(counter==firstWeekDay+1)
             {
                 monthNumbers = 1;
+            }
+            
+            //console.debug(monthNumbers,lastDay+1);
+
+            
+            
+            // Week Lines
+            if(counter%7!=0)
+            {
+                output += '\t\t'+tdOpener+monthNumbers+'</td>\n';
+            }
+            else if (counter!=0)
+            {
+                output += "\t\t"+tdOpener+monthNumbers+"</td>\n\t\t</tr>\n";
+                if(counter!=35)
+                {
+                    output+= "\t\t<tr>\n";
+                }
+            }
+
+            counter++;
+            monthNumbers++;
+            
+            if(monthNumbers==lastDay+1 && counter>28)
+            {
+                //Reset month number when month is finished
+                monthNumbers=1;
                 var resetCounter = counter;
             }
+            
+            // td openers
             if(monthNumbers==today)
             {
                 tdOpener = '\t<td class="btn-primary">';
             }
-            else if(counter<firstWeekDay)
+            else if(counter<firstWeekDay+1)
             {
                 tdOpener = '\t<td class="muted">';
             }
@@ -55,58 +99,72 @@
             {
                 tdOpener = '\t<td>';
             }
-            if(counter%7!=0)
-            {
-                output += '\t\t'+tdOpener+monthNumbers+'</td>\n';
-            }
-            else
+            
+            if (counter==limit)
             {
                 output += "\t\t"+tdOpener+monthNumbers+"</td>\n\t\t</tr>\n";
-                if(counter!=35)
-                {
-                    output+= "\t\t<tr>\n";
-                }
             }
-            counter++;
-            monthNumbers++;
-            if(counter==firstWeekDay)
-            {
-                monthNumbers = 1;
-            }
+            
         }
         output += '\t</tbody>\n</table>';
         return output;
     }
 
     $.fn.calendar = function() {
-        var month_names = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
         
-        var date = new Date(), d = date.getDate(), y = date.getFullYear(), m = date.getMonth();
-        var firstDay = new Date(y, m, 1).getDate();
-        var lastDay = new Date(y, m + 1, 0).getDate();
-        var today = d;
-
-        var firstWeekDay = getFirstDay(firstDay,m,y);
-
-        var previousMonthLastDay = new Date(y, m, 0).getDate();
         
-        var currentMonth = month_names[m];
+        var date = new Date(), day = date.getDate(), year = date.getFullYear(), month = date.getMonth();
         
         var methods = {
             init : function( ) {
-                var content = monthTable(firstWeekDay,previousMonthLastDay,lastDay, today,m,y);
+                var content = monthTable(day,month,year);
+                
                 var $this = $(this);
                 $this.html(content);
+                $('#right').click(function(e) {
+                    methods.nextMonth.call($this);
+                });
+                $('#left').click(function(e) {
+                    methods.prevMonth.call($this);
+                });
             },
-            next : function( ) {
-                var content = monthTable(firstWeekDay,previousMonthLastDay,lastDay, today,m+1,y);
+            nextMonth : function( ) {
+                month++;
+                if((month)%12==0)
+                {
+                    month=0;
+                    ++year;
+                }
+                //console.log(month,year)
+                var content = monthTable(day,month,year);
                 var $this = $(this);
+                $this.empty();
                 $this.html(content);
+                $('#right').click(function(e) {
+                    methods.nextMonth.call($this);
+                });
+                $('#left').click(function(e) {
+                    methods.prevMonth.call($this);
+                });
             },
-            prev : function( ) { 
-                var content = monthTable(firstWeekDay,previousMonthLastDay,lastDay, today,m-1,y);
+            prevMonth : function( ) {
+                --month;
+                //console.log(month,year);
+                var content = monthTable(day,month,year);
                 var $this = $(this);
+                $this.empty();
                 $this.html(content);
+                $('#right').click(function(e) {
+                    methods.nextMonth.call($this);
+                });
+                $('#left').click(function(e) {
+                    methods.prevMonth.call($this);
+                });
+                if((month)%12==0)
+                {
+                    month=12;
+                    --year;
+                }
             }
         };
         
